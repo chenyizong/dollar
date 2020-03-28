b0d194e7070002c0f09ae84a3f4b296d84394744
CHENYIZONG <136695193@qq.com>
2020-03-29 00:34:36 +0800

    手势完成

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,141 @@
 <template>
-  <div class="configure">
-    <h2>配置 configuration</h2>
-    <p><input type="checkbox"
-      id="path"
-      v-model="options.enablePath"><label for="path">开启鼠标轨迹</label><span class="option-tip">enablePath</span></p>
-    <p><span class="config-title">触发手势识别时间(ms)</span><input id="delay"
-      type="number" number lazy
-      v-model="options.timeDelay"><span class="option-tip">timeDelay</span></p>
-    <p><span class="config-title">鼠标轨迹颜色</span><input id="color"
-      type="text" lazy
-      v-model="options.lineColor"><span class="option-tip">lineColor</span></p>
-    <p><span class="config-title">鼠标轨迹宽度</span>
-      <input id="width" type="number" number lazy v-model="options.lineWidth"><span
-      class="option-tip">lineWidth</span></p>
-    <p><span class="config-title">触发手势识别鼠标按键</span><input id="key"
-      type="text" lazy
-      v-model="options.triggerMouseKey"><span class="option-tip">triggerMouseKey</span></p>
-    <p><span class="config-title">开启手势时背景色</span><input id="active-color"
-      type="text" lazy
-      v-model="options.activeColor"><span class="option-tip">triggerMouseKey</span></p>
-  </div>
-  <div class="recognize">
-    <div class="add">
-      <h2>手势区域</h2>
-      <label>添加新手势 </label>
-      <input id="gestureName" placeholder="手势名称" v-model="newGestureName">
-      <button id="btn" @click="addNewGesture">ADD</button>
-    </div>
-    <div class="result">
-      <h2>识别结果</h2>
-      <p>Gesture result: <span style="font-weight: bold; color: #2b5;" id="result">{{gestureResult}}</span></p>
-      <p>Swipe directions result: <span style="font-weight: bold; color: #2b5;" id="result0">{{swipeResult}}</span></p>
-    </div>
-    <div class="stage" v-smart-gesture="options"></div>
-    <!-- <smart-gesture id="test" class="stage"
-      :enable-path="options.enablePath"
-      :time-delay="options.timeDelay"
-      :line-color="options.lineColor"
-      :line-width="options.lineWidth"
-      :trigger-mouseKey="options.triggerMouseKey"
-      :active-color="options.activeColor">
-    </smart-gesture> -->
+  <div>
+    <br>
+    <Row>
+      <i-col :span="1"><br></i-col>
+      <i-col span="8"><div>
+        <i-input
+          type="textarea"
+          :autosize="{ minRows: 10, maxRows: 50}"
+          placeholder="请输入备忘录内容"
+          v-model="textarea2">
+        </i-input>
+        <i-table height="280" :columns="columns1" :data="data2"></i-table>
+        <br>
+        <i-button >保存备忘录</i-button>
+      </div></i-col>
+      <i-col :span="1"><br></i-col>
+      <i-col :span="13">
+        <div class="recognize">
+          <div class="add">
+            <h2>新增手势</h2>
+            <input id="gestureName" placeholder="手势名称" v-model="newGestureName" style="width: 100px"></input>
+            <i-button id="btn" @click="addNewGesture">ADD</i-button>
+          </div>
+          <div class="result">
+            <h2>识别结果</h2>
+            <p>匹配结果: <span style="font-weight: bold; color: #2b5;" id="result">{{gestureResult}}</span></p>
+            <p>滑动方向的结果: <span style="font-weight: bold; color: #2b5;" id="result0">{{swipeResult}}</span></p>
+          </div>
+          <div class="stage" v-smart-gesture="options"></div>
+        </div>
+      </i-col>
+      <i-col :span="1"><br></i-col>
+    </Row>
   </div>
 
 </template>
 
 <script>
-import smartGesture from '../lib/smart-gesture';
-
-export default {
-  data() {
-    return {
-      options: {
-        enablePath: true,
-        timeDelay: 100,
-        lineColor: '#666',
-        lineWidth: 4,
-        triggerMouseKey: 'right',
-        activeColor: 'rgba(0, 0, 0, .05)',
+  import smartGesture from '../lib/smart-gesture';
+  export default {
+    data() {
+      return {
+        columns1: [{
+          title: '手势名称',
+          key: 'name'
+        }, {
+          title: '内容',
+          key: 'context'
+        }],
+        data2: [
+          {
+            name: 'W',
+            context: '王凡是渣男！'
+          },
+          {
+            name: 'X',
+            context: '渣男是王凡！'
+          }
+        ],
+        options: {
+          enablePath: true,
+          timeDelay: 100,
+          lineColor: '#666',
+          lineWidth: 4,
+          triggerMouseKey: 'left',
+          activeColor: 'rgba(0, 0, 0, .05)',
+        },
+        swipeResult: '',
+        gestureResult: '',
+        lastPoints: [],
+        newGestureName: '',
+      };
+    },
+    directives: {
+      smartGesture
+    },
+    methods: {
+      addNewGesture() {
+        if (!this.newGestureName) {
+          alert('给我起个名字啊！');
+          return;
+        }
+        this.$smartGesture.addGesture({
+          name: this.newGestureName,
+          points: this.lastPoints,
+        });
       },
-      swipeResult: '',
-      gestureResult: '',
-      lastPoints: [],
-      newGestureName: '',
-    };
-  },
-  directives: {
-    smartGesture
-  },
-  methods: {
-    addNewGesture() {
-      if (!this.newGestureName) {
-        alert('给我起个名字啊！');
-        return;
-      }
-      this.$smartGesture.addGesture({
-        name: this.newGestureName,
-        points: this.lastPoints,
-      });
+      'smart-gesture-onswipe': function(list) {
+        this.swipeResult = list;
+      },
+      'smart-gesture-ongesture': function(res, points) {
+        this.gestureResult = res.score > 2 ? res.name : '未识别';
+        this.lastPoints = points;
+      },
     },
-    'smart-gesture-onswipe': function(list) {
-      this.swipeResult = list;
-    },
-    'smart-gesture-ongesture': function(res, points) {
-      this.gestureResult = res.score > 2 ? res.name : '未识别';
-      this.lastPoints = points;
-    },
-  },
-  events: {
-    'smart-gesture-onswipe': function(list) {
-      this.swipeResult = list;
-    },
-    'smart-gesture-ongesture': function(res, points) {
-      this.gestureResult = res.score > 2 ? res.name : '未识别';
-      console.log(res);
-      this.lastPoints = points;
-    },
+    events: {
+      'smart-gesture-onswipe': function(list) {
+        this.swipeResult = list;
+      },
+      'smart-gesture-ongesture': function(res, points) {
+        this.gestureResult = res.score > 2 ? res.name : '未识别';
+        this.lastPoints = points;
+      },
+    }
   }
-};
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-  .configure,
+  .i-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .i-button{
+    margin-top: 5px;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
   .recognize {
-    border: 1px solid #ccc;
+    border: 5px solid #ccc;
     border-radius: 4px;
     padding: 0 15px;
-    margin-top: 10px;
+    margin-top: 0px;
   }
 
   .add,
@@ -134,14 +163,14 @@ export default {
 
   .stage {
     height: 400px;
-    background: #ddd;
+    background:#f5f7f9;
     position: relative;
     margin: 15px 0;
   }
 
   .stage:before {
-    content: '绘制区域';
-    color: #eeeeee;
+    content: '手势输入';
+    color: #c3cbd6;
     font-size: 30px;
     position: absolute;
     top: 50%;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
-
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 /* eslint-disable no-new */
+Vue.use(iView);
 new Vue({
   el: 'body',
   components: { App }
